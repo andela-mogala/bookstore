dfc130f7fc1232b903a9568ee5233dd9f45e241e
Michael Ogala <michael.ogala@andela.com>
2017-05-13 13:14:12 +0100

    configure models and serializers

--- app/models/publisher.js
+++ app/models/publisher.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: BS.attr('string'),
+  name: DS.attr('string'),
   discount: DS.attr('number'),
   published: DS.hasMany('book')
 });
